e24f75b2ca3f6730b1b20f1cbc73880b320dc58b
suttegi <esenerlan06@gmail.com>
2025-02-09 10:50:31 +0500

    [feat]: lab3 todo is done

---
+++ Lab3/script.js
@@ -0,0 +1,38 @@
+const handleAddTask = () => {
+    const taskContainer = document.querySelector(".task_container");
+    const taskInput = document.querySelector("textarea");
+
+    if (taskContainer && taskInput.value.trim() !== "") {
+        const newTask = document.createElement("li");
+        newTask.classList.add("task-item");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("isDone");
+
+        const taskText = document.createElement("span");
+        taskText.textContent = taskInput.value;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "del";
+        deleteButton.classList.add("delete-task");
+        deleteButton.onclick = () => newTask.remove();
+
+        newTask.append(checkbox, taskText, deleteButton);
+        taskContainer.appendChild(newTask);
+        taskInput.value = "";
+    }
+};
+
+const handleTaskIsDone = () => {
+    document.querySelectorAll(".isDone").forEach((checkbox) => {
+        const taskItem = checkbox.parentElement;
+        if (checkbox.checked) {
+            taskItem.classList.add("done");
+        } else {
+            taskItem.classList.remove("done");
+        }
+    });
+};
+
+document.addEventListener("change", handleTaskIsDone);
